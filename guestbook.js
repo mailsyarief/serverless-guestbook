b9a0c435c121c5b5a84bd6b59b3705c9557f7d99
mailsyarief <ismail8syarief@gmail.com>
2021-04-27 19:09:57 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5ca2d819.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
